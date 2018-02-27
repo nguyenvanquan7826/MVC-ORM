5573903792ffd1336b19d945594ec6da7c3f8a71
nguyenvanquan7826 <nguyenvanquan7826@gmail.com>
2018-02-27 12:40:44 +0700

    Edit struct for users table

--- vietlach_app.sql
+++ vietlach_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 27, 2018 at 12:23 PM
+-- Generation Time: Feb 27, 2018 at 01:40 PM
 -- Server version: 10.1.24-MariaDB-cll-lve
 -- PHP Version: 5.6.30
 
@@ -30,17 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mvcorm_user` (
   `id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` text NOT NULL
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mvcorm_user`
 --
 
-INSERT INTO `mvcorm_user` (`id`, `email`, `password`) VALUES
-(22, 'abcde@cd', '12'),
-(21, 'abcdf@cd', '12');
+INSERT INTO `mvcorm_user` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Nguyễn Văn Quân', 'nguyenvanquan7826@gmail.com', '123'),
+(2, 'My Love', 'mylove@gmail.com', '12');
 
 --
 -- Indexes for dumped tables
