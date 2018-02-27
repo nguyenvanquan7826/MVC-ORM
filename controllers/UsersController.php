17a4a7ac3a73df2d697fbdc0d79bb05c8cf0be6a
nguyenvanquan7826 <nguyenvanquan7826@gmail.com>
2018-02-27 23:59:02 +0700

    update logic register

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -26,11 +26,22 @@ class UsersController extends Controller {
             if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password'])) {
                 return false;
             }
-            $result = User::create([
-                'name' => $_POST['name'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password'],
-            ]);
+
+            if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['password'])) {
+                $this->data['saved'] = 'Please enter name, email, password';
+                return false;
+            }
+
+            try {
+                $result = User::create([
+                    'name' => $_POST['name'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password'],
+                ]);
+            } catch (Exception $e) {
+                $this->data['saved'] = $e->getMessage();
+                return false;
+            }
             if ($result) {
                 $this->data['saved'] = 'Register success';
             }
